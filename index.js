cc4d8b04a3ee30847c2bc4cd5a9d80f27784cd86
Gonzalo Bartolini <gonzaelchino200957@gmail.com>
2021-08-30 12:52:16 -0300

    primer commit del proyecto final

---
+++ index.js
@@ -0,0 +1,17 @@
+const list = document.querySelector(".list");
+const menu = document.querySelector(".menu");
+
+list.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+
+// mostrar submenu
+
+// const item = document.querySelector(".ul__item");
+// const sub = Document.getElementByClass(".sub");
+
+
+// item.addEventListener("click", () => {
+//     sub.classList.toggle("mostrar");
+// })
