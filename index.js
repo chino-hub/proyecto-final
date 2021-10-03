39bc201e56e3f310d322b0c80d1c4ef29c417546
Gonzalo Bartolini <gonzaelchino200957@gmail.com>
2021-10-03 16:15:15 -0300

    estilos-desktop

--- index.js
+++ index.js
@@ -4,10 +4,3 @@ const menu = document.querySelector(".menu");
 list.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
-
-const listGenero = document.querySelector(".busqueda__list");
-const genero = Document.querySelector(".mostrar");
-
-listGenero.addEventListener("click", () => {
-    genero.classList("genero");
-})
